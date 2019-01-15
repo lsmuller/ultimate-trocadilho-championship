372cc01e28345b8b1c2924b3e0d02003e0c789e2
Leticia Muller <leticia.muller@sap.com>
2019-01-15 21:20:39 -0200

    created project

---
+++ src/main/java/com/utc/UtcApplication.java
@@ -0,0 +1,14 @@
+package com.utc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtcApplication.class, args);
+	}
+
+}
+
